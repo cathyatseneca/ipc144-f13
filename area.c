22deeddee820fa7963abe34ab3f901dccd0ed036
Catherine Leung <catherine.leung@senecacollege.ca>
2013-09-10 15:29:03 -0400

    sept 10

--- area.c
+++ area.c
@@ -14,4 +14,20 @@
 */
 #include <stdio.h>
 int main(void){
+  int base;
+  int height;
+  double area;
+  printf("please enter the base: ");
+  scanf("%d",&base);
+  printf("please enter the height: ");
+  scanf("%d",&height);
+  
+  area=(base*height)/2.0;
+ //alternatively: area=((double)(base*height))/2;
+  printf("The area of a triangle with base of %d and height of %d is: ",base, height);
+  //below will show on same line as prev statement
+  //the format code %lf means long float (a double) and the .2 means
+  //show to 2 decimal place
+  printf("%.2lf\n",area);
+  return 0;
 }
