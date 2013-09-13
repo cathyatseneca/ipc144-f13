541ede85901aa7638eaf239d956207d71f27c961
Catherine Leung <catherine.leung@senecacollege.ca>
2013-09-13 13:34:24 -0400

    sept 13

---
+++ cone.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*this is a constant... PI can be used like a variable in your
+  program.  Its value will be 3.14159*/
+
+#define PI 3.14159
+
+/*this program asks the user for 2 values representing the base and height 
+both floating point.  It will then calculate the volume of a cone of that
+size and print the result.
+*/
+int main(void){
+}
