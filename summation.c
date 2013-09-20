cbdde7d8c926aeac56e73146ae051f377f43511e
Catherine Leung <catherine.leung@senecacollege.ca>
2013-09-20 13:33:38 -0400

    Sept 20

---
+++ summation.c
@@ -0,0 +1,13 @@
+/*
+This program will sum the values you entered by a user.  The program 
+will use a non-positive value to indicate that it is done.  That is if the 
+user enters a positive value, the program adds it to the sum.  If the user 
+enters a non-positive value (0 or less) it terminates the program. At 
+the end the program prints the sum of the values entered
+*/
+
+int main(void){
+
+  return 0;
+}
+
