a2019c9b3ad8b2c52896b7428a0cfaba50f89a3a
Catherine Leung <cathyatseneca@gmail.com>
2013-10-07 13:35:14 -0400

    oct 7

--- change.c
+++ change.c
@@ -1,3 +1,8 @@
+/*this function passed a monetary value in cents and it rounds that money to 
+  the nearest nickel according to the rules.  That is for values ending with
+  1,2,6,7 round down, and 3,4,8,9 round up.  This value is returned*/
+int roundToNearestNickel(int amt){
+}
 /* this function is passed an int representing the amount of change
    we want to make, and we need to determine how many of each type
    of coins is necessary to make that change.
@@ -7,10 +12,12 @@
   pointers that hold the addresses of variable where we wish to store
   that information
 */
-
 void getChange(int amt,int* numToonies, int* numLoonies,
 			int* numQuarters,int* numDimes, int* numNickels){
 
+  //round amt to nearest nickel
+  amt = roundToNearestNickel(amt);
+  *numToonies = amt/200;
 }
 int main(void){
   int loonies;
