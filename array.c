af7cf7e0e4856028a17ce0479ecce3366b489e8e
Catherine Leung <cathyatseneca@gmail.com>
2013-10-18 14:02:03 -0400

    Oct 18, 2013

--- array.c
+++ array.c
@@ -9,7 +9,7 @@ int main(void){
 	int arr[MAX] = {3,7,10,14,2,1,16,15,12,5};
 	int arr2[MAX/2];
 	for(i=0;i<MAX/2;i++){
-		arr[i]=i+1;
+		arr2[i]=i+1;
 	}
 	printArray(arr,MAX);
 	printArray(arr2,MAX/2);
